a77a3ce5c5674a23cf632a8deb8dc2514823c979
NicsSuazo2003 <nicssuazo@gmail.com>
2026-05-25 13:54:47 +0800

    Fix DB connection and skip migrations

--- Program.cs
+++ Program.cs
@@ -58,7 +58,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
     DbSeeder.Initialize(db);
 }
 
