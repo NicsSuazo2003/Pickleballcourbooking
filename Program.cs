14d2450599c4de9ff4d32e1ac76d827af3cf1c01
NicsSuazo2003 <nicssuazo@gmail.com>
2026-05-25 14:16:43 +0800

    fix: run migrations on startup

--- Program.cs
+++ Program.cs
@@ -58,6 +58,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate(); // ← Add this line
+
     DbSeeder.Initialize(db);
 }
 
