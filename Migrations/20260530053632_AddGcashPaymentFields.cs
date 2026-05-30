35a839186c43b5d213e6945b48f25fc4a1c50a39
NicsSuazo2003 <nicssuazo@gmail.com>
2026-05-30 13:36:40 +0800

    Add GCash payment migration

---
+++ Migrations/20260530053632_AddGcashPaymentFields.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PickleballBookingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGcashPaymentFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PaymentExpiresAt",
+                table: "bookings",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentScreenshot",
+                table: "bookings",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentExpiresAt",
+                table: "bookings");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentScreenshot",
+                table: "bookings");
+        }
+    }
+}
